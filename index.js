1bae70fb9f78936fc9953900e95dcad9401aadc9
Estelle Moncollin <estelle.moncollin.ext@cgi.com>
2018-12-02 21:44:40 +0100

    master

--- index.js
+++ index.js
@@ -3,17 +3,20 @@ $(function() {
     console.log("[DEMO] :: Rainbow Application started!");
 
     var applicationID = "",
-        applicationSecret = "";
+     applicationSecret = "";
 
     /* Bootstrap the SDK */
     angular.bootstrap(document, ["sdk"]).get("rainbowSDK");
 
+    /* Callback for handling the event 'RAINBOW_ONREADY' */
+    var onReady = function onReady() {
+        console.log("[DEMO] :: On SDK Ready !");
+        // do something when the SDK is ready
+    };
+
     /* Callback for handling the event 'RAINBOW_ONCONNECTIONSTATECHANGED' */
     var onLoaded = function onLoaded() {
         console.log("[DEMO] :: On SDK Loaded !");
-
-        var applicationID = "b50443d0f10b11e8a85b99fd800e70f8",
-        applicationSecret = "2ghU9D5e2pR7YtU2rY3byIcvox6J3brTu8gWzJ7o0HFxdWbyl9B5lqw2tz5ta5eW";
 
         // Activate full SDK log
         rainbowSDK.setVerboseLog(true);
@@ -28,31 +31,12 @@ $(function() {
             });
     };
 
+    /* Listen to the SDK event RAINBOW_ONREADY */
+    $(document).on(rainbowSDK.RAINBOW_ONREADY, onReady);
+
     /* Listen to the SDK event RAINBOW_ONLOADED */
     $(document).on(rainbowSDK.RAINBOW_ONLOADED, onLoaded);
 
     /* Load the SDK */
     rainbowSDK.load();
-
-    var onReady = function onReady() {
-
-        var myRainbowLogin = "estelle.moncollin@gmail.com";       // Replace by your login
-        var myRainbowPassword = "Rainbow123!*"; // Replace by your password
-
-        // The SDK for Web is ready to be used, so you can sign in
-        rainbowSDK.connection.signin(myRainbowLogin, myRainbowPassword)
-        .then(function(account) {
-              // Successfully signed to Rainbow and the SDK is started completely. Rainbow data could be retrieved.
-              ...
-        })
-        .catch(function(err) {
-              // An error occurs (e.g. bad credentials). Application could be informed that sign-in has failed
-              ...
-        });
-    };
-
-    /* Listen to the SDK event RAINBOW_ONREADY */
-    $(document).on(rainbowSDK.RAINBOW_ONREADY, onReady);
-
-
 });
